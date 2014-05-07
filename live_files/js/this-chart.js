d4b24a0d630f16380c9903fc90a45b8080493b6c
muranava <navabcde1@yahoo.co.uk>
2014-05-07 10:15:18 +0200

    Update this-chart.js

--- live_files/js/this-chart.js
+++ live_files/js/this-chart.js
@@ -144,7 +144,7 @@ function update_bingo()
 
     if(bingo)
     {
-     $('#yell-link').attr('href', 'https://twitter.com/intent/tweet?hashtags=debatebingo&url='+encodeURI(key_to_link())+"&text="+encodeURIComponent("BINGO!!! Check out my bingo card and make your own"));
+     $('#yell-link').attr('href', 'https://twitter.com/intent/tweet?hashtags=elttalkbingo&url='+encodeURI(key_to_link())+"&text="+encodeURIComponent("BINGO!!! Check out my bingo card and make your own"));
         $('#yell-button').attr('src', 'img/yellbingo_active.png');
 
     }
